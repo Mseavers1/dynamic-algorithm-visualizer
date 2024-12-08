a3e36be1e1a548b52150de3f9cde65c6a338de93
Mseavers1 <91432334+Mseavers1@users.noreply.github.com>
2024-12-08 00:45:21 -0600

    No select for texts

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -16,10 +16,10 @@ function App() {
             <div className="absolute inset-0 bg-radial-noise opacity-20 pointer-events-none"></div>
 
             {/** Title **/}
-            <h1 className="font-playfair text-wkuYellow text-7xl md:text-8xl font-bold mb-8 drop-shadow-[5px_10px_12px_rgba(0,0,0,0.4)]">
+            <h1 className="select-none font-playfair text-wkuYellow text-7xl md:text-8xl font-bold mb-8 drop-shadow-[5px_10px_12px_rgba(0,0,0,0.4)]">
                 D.A.V.E.
             </h1>
-            <p className="font-roboto text-sm sm:text-base md:text-lg lg:text-2xl text-white font-medium mb-16 tracking-widest-two drop-shadow-[5px_10px_12px_rgba(0,0,0,0.2)]">
+            <p className="select-none font-roboto text-sm sm:text-base md:text-lg lg:text-2xl text-white font-medium mb-16 tracking-widest-two drop-shadow-[5px_10px_12px_rgba(0,0,0,0.2)]">
                 A <span className="font-playfair font-semibold text-wkuYellow">D</span>ynamic <span
                 className="font-playfair font-semibold text-wkuYellow">A</span>lgorithm <span
                 className="font-playfair font-semibold text-wkuYellow">V</span>isualizer for <span
@@ -27,7 +27,7 @@ function App() {
             </p>
 
             {/** Buttons **/}
-            <div className="flex space-x-4 md:space-x-6 lg:space-x-8 md:text-2xl mb-8">
+            <div className="select-none flex space-x-4 md:space-x-6 lg:space-x-8 md:text-2xl mb-8">
                 <button
                     className="font-roboto bg-transparent text-gray-300 px-6 py-3 rounded-md hover:text-wkuYellow hover:scale-105 transition-all drop-shadow-[3px_3px_2px_rgba(0,0,0,0.5)]">
                     About

1ff2b92effe244f799d36e6322effbad53576252
Mseavers1 <91432334+Mseavers1@users.noreply.github.com>
2025-02-10 09:31:00 -0600

    Update animator.tsx

--- app/src/components/animator.tsx
+++ app/src/components/animator.tsx
@@ -22,7 +22,7 @@ const Animator: React.FC = () => {
                     onChange={(e) => setValue(e.target.value)}
                 />
                 <button className="px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-600 active:scale-90 transition transform duration-150 ease-out focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
-                        onClick={() => {algorithm?.insert(value)}}>
+                        onClick={() => {algorithm?.insert(value); setValue("")}}>
                     Insert
                 </button>
                 <button className="px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-600 active:scale-90 transition transform duration-150 ease-out focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
